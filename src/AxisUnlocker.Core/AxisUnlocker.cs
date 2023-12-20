51957730a37d6a20d79b61142aae9e6715921d70
Tamás Márkus <markus.tamas98@gmail.com>
2023-12-21 00:28:45 +0100

    [AxisUnlocker] Add number display while editing via shortcut

--- src/AxisUnlocker.Core/AxisUnlocker.cs
+++ src/AxisUnlocker.Core/AxisUnlocker.cs
@@ -25,15 +25,23 @@ namespace AxisUnlocker.Koikatu {
         public static ConfigEntry<float> NewMinSize { get; set; }
         public static ConfigEntry<float> NewMaxSize { get; set; }
         public static ConfigEntry<bool> UseLogSize { get; set; }
+        public static ConfigEntry<bool> DisplaySizeWhileEditing { get; set; }
         public static ConfigEntry<float> NewMinMove { get; set; }
         public static ConfigEntry<float> NewMaxMove { get; set; }
         public static ConfigEntry<bool> UseLogMove { get; set; }
+        public static ConfigEntry<bool> DisplayMoveWhileEditing { get; set; }
         public static ConfigEntry<KeyboardShortcut> Hotkey { get; private set; }
+
+        internal static GameObject guiContainer;
+        internal static SimpleDisplay display;
+
+        private static Vector2 displaySize = new Vector2(80,45);
 
         private void Awake() {
             // Options
             UseLogSize = Config.Bind("General", "Logarithmic axis size", true, new ConfigDescription("Use a logarithmic slider for the guideobject scaling factor. This enables finer control across very different sizes.", null, new ConfigurationManagerAttributes { Order = 4 }));
             UseLogSize.SettingChanged += (x, y) => UpdateSliders(_logSizeChanged:true);
+            DisplaySizeWhileEditing = Config.Bind("General", "Show axis size", true, new ConfigDescription("Show the current value of the slider in a box above the cursor while using the B + Right click-drag shortcut", null, new ConfigurationManagerAttributes { Order = 4 }));
 
             NewMinSize = Config.Bind("General", "New min size", 0.1f, new ConfigDescription("New cap value for the System/Options/Size slider", new AcceptableValueRange<float>(0.01f, 1f), new ConfigurationManagerAttributes { Order = 3 }));
             NewMinSize.SettingChanged += (x, y) => UpdateSliders();
@@ -42,6 +50,7 @@ namespace AxisUnlocker.Koikatu {
 
             UseLogMove = Config.Bind("General", "Logarithmic move speed", true, new ConfigDescription("Use a logarithmic slider for the object move speed factor. This enables finer control for very different speeds.", null, new ConfigurationManagerAttributes { Order = 2 }));
             UseLogMove.SettingChanged += (x, y) => UpdateSliders(_logMoveChanged:true);
+            DisplayMoveWhileEditing = Config.Bind("General", "Show move speed", true, new ConfigDescription("Show the current value of the slider in a box above the cursor while using the B + Left click-drag shortcut", null, new ConfigurationManagerAttributes { Order = 2 }));
 
             NewMinMove = Config.Bind("General", "New min speed", 0.01f, new ConfigDescription("New minimum value for the System/Options/Speed slider", new AcceptableValueRange<float>(0.001f, 0.1f), new ConfigurationManagerAttributes { Order = 1 }));
             NewMinMove.SettingChanged += (x, y) => UpdateSliders();
@@ -50,6 +59,9 @@ namespace AxisUnlocker.Koikatu {
 
             Hotkey = Config.Bind("Hidden", "Move change hotkey", new KeyboardShortcut(KeyCode.B), new ConfigDescription("B", null, new ConfigurationManagerAttributes { Order = 0, Browsable = false }));
 
+            // Setup KKAPI
+            KKAPI.Studio.StudioAPI.StudioLoadedChanged += (x, y) => MakeGUI();
+
             // Setup Harmony hooks
             HookPatch.Init();
 #if DEBUG
@@ -57,10 +69,48 @@ namespace AxisUnlocker.Koikatu {
 #endif
         }
 
+        private void MakeGUI() {
+            Transform parent = Singleton<Studio.Studio>.Instance.rootButtonCtrl.gameObject.transform.GetChild(0);
+            guiContainer = new GameObject("AxisUnlockerWindow");
+            guiContainer.transform.SetParent(parent);
+            display = guiContainer.AddComponent<SimpleDisplay>();
+            display.SetSize(new Vector2(80,45));
+        }
+
         private void Update() {
-            if (Hotkey.Value.IsPressed()) {
-                if (Input.GetMouseButton(0)) {
-                    ChangeMove();
+            if (display != null) {
+                if (Hotkey.Value.IsPressed()) {
+
+                    // Changing movement speed
+                    if (Input.GetMouseButtonDown(0) && DisplayMoveWhileEditing.Value) {
+                        display.SetName("Move Speed");
+                        display.Show(true);
+                    }
+                    if (Input.GetMouseButton(0)) {
+                        ChangeMove();
+                        display.SetData(Studio.Studio.optionSystem.manipuleteSpeed);
+                    }
+
+                    // Changing axis size
+                    if (Input.GetMouseButtonDown(1) && DisplaySizeWhileEditing.Value) {
+                        display.SetName("Axis Size");
+                        display.Show(true);
+                    }
+                    if (Input.GetMouseButton(1)) {
+                        display.SetData(Studio.Studio.optionSystem.manipulateSize);
+                    }
+
+                    // Showing GUI
+                    if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
+                        display.SetPos(new Vector2(Input.mousePosition.x - displaySize.x / 2, Screen.height - Input.mousePosition.y - displaySize.y - 3));
+                    }
+                }
+
+                // Hiding GUI
+                if (display.IsOn) {
+                    if (!Input.GetMouseButton(0) && !Input.GetMouseButton(1)) {
+                        display.Show(false);
+                    }
                 }
             }
         }
---
+++ src/AxisUnlocker.Core/Graphics.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace AxisUnlocker.Koikatu {
+    class SimpleDisplay : MonoBehaviour {
+        public bool IsOn { get; private set; }
+        public Vector2 Size { get; private set; }
+        public Vector2 Pos { get; private set; }
+
+        private string data = "";
+        private string dispName = "";
+        private Color dispColor = Color.white;
+        private readonly GUIStyle textStyle;
+
+        public SimpleDisplay() {
+            this.Size = new Vector2(120, 40);
+            this.Pos = Vector2.zero;
+            textStyle = GUIStyle.none;
+            textStyle.alignment = TextAnchor.UpperCenter;
+            textStyle.normal.textColor = this.dispColor;
+        }
+
+        public void Show(bool _show) {
+            this.IsOn = _show;
+        }
+
+        public void SetData(float _num) {
+            this.data = _num.ToString("0.00");
+        }
+
+        public void SetName(string _name) {
+            this.dispName = _name;
+        }
+
+        public void SetSize(Vector2 _newSize) {
+            this.Size = _newSize;
+        }
+
+        public void SetPos(Vector2 _newPos) {
+            this.Pos = _newPos;
+        }
+
+        private void OnGUI() {
+            if (IsOn) {
+                GUI.Box(new Rect(Pos.x, Pos.y, Size.x, Size.y), this.dispName);
+                GUI.Label(new Rect(Pos.x, Pos.y + 25, Size.x, Size.y), this.data, textStyle);
+            }
+        }
+    }
+}
