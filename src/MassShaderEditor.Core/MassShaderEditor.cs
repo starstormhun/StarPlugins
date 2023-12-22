12eb90d1b55e353bece7fdbf14fd9acf1b992e9d
Tamás Márkus <markus.tamas98@gmail.com>
2023-12-22 23:57:37 +0100

    [MassShaderEditor] Add toggleable UI, drag enabled

--- src/MassShaderEditor.Core/MassShaderEditor.cs
+++ src/MassShaderEditor.Core/MassShaderEditor.cs
@@ -20,14 +20,33 @@ namespace MassShaderEditor.Koikatu {
         public const string GUID = "starstorm.massshadereditor";
         public const string Version = "1.0.0." + BuildNumber.Version;
 
-        private void Awake() {
-#if DEBUG
-            Logger.LogInfo($"Plugin {GUID} is loaded!");
-#endif
+        private static ConfigEntry<KeyboardShortcut> VisibleHotkey { get; set; }
+
+        private static bool isShown = false;
+        private Rect windowRect = new Rect(500, 40, 240, 170);
+
+        private void Start() {
+            VisibleHotkey = Config.Bind("General", "UI Toggle", new KeyboardShortcut(KeyCode.M), new ConfigDescription("The key used to toggle the plugin's UI",null,new KKAPI.Utilities.ConfigurationManagerAttributes{ Order = 10}));
         }
 
         private void Update() {
-			
+            if (VisibleHotkey.Value.IsDown())
+                isShown = !isShown;
+            if (!KKAPI.Maker.MakerAPI.InsideMaker && !KKAPI.Studio.StudioAPI.InsideStudio)
+                isShown = false;
+        }
+
+        private void OnGUI() {
+            if (isShown) {
+                windowRect = GUI.Window(587, windowRect, WindowFunction, "Mass Shader Editor v" + Version);
+                KKAPI.Utilities.IMGUIUtils.EatInputInRect(windowRect);
+            }
+        }
+
+        private void WindowFunction(int WindowID) {
+
+
+            GUI.DragWindow();
         }
     }
 }
