48752014fa0f85cb075abad343f127eddcb634c7
starstormhun <markus.tamas98@gmail.com>
2024-12-09 00:46:41 +0100

    First semi-functional DynamicBone optimisation

--- src/Performancer.Core/HookPatch.cs
+++ src/Performancer.Core/HookPatch.cs
@@ -15,12 +15,17 @@ namespace Performancer {
             HookPatch.Hooks.UnregisterHooks();
         }
 
-        private static class Hooks {
+        internal static class Hooks {
             private static Harmony _harmony;
+
+            private const int frameAllowance = 6;
 
             private static Dictionary<Transform, GuideObject> dicGuideObjects = new Dictionary<Transform, GuideObject>();
             private static Dictionary<GuideObject, Dictionary<string, Vector3>> dicGuideObjectVals = new Dictionary<GuideObject, Dictionary<string, Vector3>>();
+            internal static Dictionary<MonoBehaviour, Dictionary<string, object>> dicDynBoneVals = new Dictionary<MonoBehaviour, Dictionary<string, object>>();
+
             private static Dictionary<GuideObject, bool> dicGuideObjectsToUpdate = new Dictionary<GuideObject, bool>();
+            private static Dictionary<MonoBehaviour, int> dicDynBonesToUpdate = new Dictionary<MonoBehaviour, int>();
 
             // Setup Harmony and patch methods
             public static void SetupHooks() {
@@ -79,6 +84,16 @@ namespace Performancer {
                                         dicGuideObjectsToUpdate[dicGuideObjects[curr]] = true;
                                     }
                                     iterateList.AddRange(curr.Children());
+
+                                    if (Performancer.OptimiseDynamicBones.Value) {
+                                        var dynBones = new List<MonoBehaviour>();
+                                        dynBones.AddRange(curr.GetComponents<DynamicBone>());
+                                        dynBones.AddRange(curr.GetComponents<DynamicBone_Ver02>());
+                                        foreach (var dynBone in dynBones) {
+                                            // I leave some leeway for the bone to start/stop moving
+                                            dicDynBonesToUpdate[dynBone] = frameAllowance;
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -93,6 +108,103 @@ namespace Performancer {
                     Performancer.numGuideObjectLateUpdates++;
                 }
                 return result;
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(DynamicBone), "Start")]
+            [HarmonyPatch(typeof(DynamicBone_Ver02), "Awake")]
+            private static void DynamicBonesAfterCreate(ref MonoBehaviour __instance) {
+                // Register bones when spawned
+                dicDynBoneVals[__instance] = new Dictionary<string, object> {
+                    { "enabled", __instance.isActiveAndEnabled }
+                };
+                switch (__instance) {
+                    case DynamicBone db:
+                        dicDynBoneVals[__instance]["pos"] = db.m_Particles[db.m_Particles.Count - 1].m_Position;
+                        break;
+                    case DynamicBone_Ver02 db:
+                        dicDynBoneVals[__instance]["pos"] = db.Particles[db.Particles.Count - 1].Position;
+                        break;
+                }
+
+                // Allow bones to settle when spawned
+                dicDynBonesToUpdate[__instance] = 2*frameAllowance;
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(DynamicBone), "LateUpdate")]
+            [HarmonyPatch(typeof(DynamicBone_Ver02), "LateUpdate")]
+            private static bool DynamicBonesBeforeLateUpdate(ref MonoBehaviour __instance) {
+                bool result;
+                // If we don't optimise, then always run the Update scripts
+                if (!Performancer.OptimiseGuideObjectLate.Value || !Performancer.OptimiseDynamicBones.Value) {
+                    result = true;
+                // If there's leeway time left, continue running
+                } else if (dicDynBonesToUpdate.TryGetValue(__instance, out int framesLeft) && framesLeft > 0) {
+                    result = true;
+                // If the component was just turned on, add leeway frames
+                } else if (dicDynBoneVals[__instance]["enabled"] is bool enabled && !enabled && __instance.isActiveAndEnabled) {
+                    dicDynBonesToUpdate[__instance] = frameAllowance;
+                    result = true;
+                } else {
+                    result = false;
+                }
+
+                // Update saved values
+                dicDynBoneVals[__instance]["enabled"] = __instance.isActiveAndEnabled;
+                switch (__instance) {
+                    case DynamicBone db:
+                        if (db.m_Particles.Count > 0) {
+                            dicDynBoneVals[__instance]["pos"] = db.m_Particles[db.m_Particles.Count - 1].m_Position;
+                        } else {
+                            dicDynBoneVals[__instance]["pos"] = Vector3.zero;
+                        }
+                        break;
+                    case DynamicBone_Ver02 db:
+                        if (db.Particles.Count > 0) {
+                            dicDynBoneVals[__instance]["pos"] = db.Particles[db.Particles.Count - 1].Position;
+                        } else {
+                            dicDynBoneVals[__instance]["pos"] = Vector3.zero;
+                        }
+                        break;
+                }
+
+                if (!result) {
+                    switch (__instance) {
+                        case DynamicBone db: db.ApplyParticlesToTransforms(); break;
+                        case DynamicBone_Ver02 db: db.ApplyParticlesToTransforms(); break;
+                    }
+                }
+
+                return result;
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(DynamicBone), "LateUpdate")]
+            [HarmonyPatch(typeof(DynamicBone_Ver02), "LateUpdate")]
+            private static void DynamicBonesAfterUpdate(ref MonoBehaviour __instance) {
+                // If we don't optimise, then skip the postfix
+                if (!Performancer.OptimiseGuideObjectLate.Value || !Performancer.OptimiseDynamicBones.Value) {
+                    return;
+                }
+
+                // Get current position of bone
+                Vector3 pos = Vector3.zero;
+                switch (__instance) {
+                    case DynamicBone db:
+                        if (db.m_Particles.Count > 0) pos = db.m_Particles[db.m_Particles.Count - 1].m_Position;
+                        break;
+                    case DynamicBone_Ver02 db:
+                        if (db.Particles.Count > 0) pos = db.Particles[db.Particles.Count - 1].Position;
+                        break;
+                }
+
+                // If it didn't change during the update, subtract from the frame allowance
+                if (dicDynBoneVals[__instance]["pos"] is Vector3 dicPos && dicPos == pos) {
+                    if (dicDynBonesToUpdate.TryGetValue(__instance, out int frames) && frames > 0) {
+                        dicDynBonesToUpdate[__instance] = frames - 1;
+                    }
+                }
             }
         }
     }
