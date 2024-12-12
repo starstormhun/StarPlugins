41d3fd6889fd216d5238fa9ac094c2eb871f58f0
starstormhun <markus.tamas98@gmail.com>
2024-12-12 23:46:48 +0100

    Add support for _Ver01, add collider change detection

--- src/Performancer.Core/HookPatch.cs
+++ src/Performancer.Core/HookPatch.cs
@@ -18,14 +18,15 @@ namespace Performancer {
         internal static class Hooks {
             private static Harmony _harmony;
 
-            private const int frameAllowance = 3;
+            internal const int frameAllowance = 3;
 
             private static Dictionary<Transform, GuideObject> dicGuideObjects = new Dictionary<Transform, GuideObject>();
+
             private static Dictionary<GuideObject, Dictionary<string, Vector3>> dicGuideObjectVals = new Dictionary<GuideObject, Dictionary<string, Vector3>>();
             internal static Dictionary<MonoBehaviour, Dictionary<string, Vector3>> dicDynBoneVals = new Dictionary<MonoBehaviour, Dictionary<string, Vector3>>();
 
             private static Dictionary<GuideObject, bool> dicGuideObjectsToUpdate = new Dictionary<GuideObject, bool>();
-            private static Dictionary<MonoBehaviour, int> dicDynBonesToUpdate = new Dictionary<MonoBehaviour, int>();
+            internal static Dictionary<MonoBehaviour, int> dicDynBonesToUpdate = new Dictionary<MonoBehaviour, int>();
 
             // Setup Harmony and patch methods
             public static void SetupHooks() {
@@ -35,6 +36,11 @@ namespace Performancer {
             // Disable Harmony patches of this plugin
             public static void UnregisterHooks() {
                 _harmony.UnpatchSelf();
+            }
+
+            internal static void ClearDynBoneDics() {
+                dicDynBoneVals.Clear();
+                dicDynBonesToUpdate.Clear();
             }
 
             [HarmonyPrefix]
@@ -88,6 +94,7 @@ namespace Performancer {
                                     if (Performancer.OptimiseDynamicBones.Value) {
                                         var dynBones = new List<MonoBehaviour>();
                                         dynBones.AddRange(curr.GetComponents<DynamicBone>());
+                                        dynBones.AddRange(curr.GetComponents<DynamicBone_Ver01>());
                                         dynBones.AddRange(curr.GetComponents<DynamicBone_Ver02>());
                                         foreach (var dynBone in dynBones) {
                                             // I leave some leeway for the bone to start/stop moving
@@ -112,11 +119,13 @@ namespace Performancer {
 
             [HarmonyPostfix]
             [HarmonyPatch(typeof(DynamicBone), "Start")]
+            [HarmonyPatch(typeof(DynamicBone_Ver01), "Start")]
             [HarmonyPatch(typeof(DynamicBone_Ver02), "Awake")]
             private static void DynamicBonesAfterCreate(ref MonoBehaviour __instance) {
                 // Register bones when spawned
                 dicDynBoneVals[__instance] = new Dictionary<string, Vector3> {
                     { "pos", Vector3.zero },
+                    { "tfPos", Vector3.zero },
                     { "force", Vector3.zero },
                     { "gravity", Vector3.zero },
                 };
@@ -127,19 +136,27 @@ namespace Performancer {
 
             [HarmonyPrefix]
             [HarmonyPatch(typeof(DynamicBone), "LateUpdate")]
+            [HarmonyPatch(typeof(DynamicBone_Ver01), "LateUpdate")]
             [HarmonyPatch(typeof(DynamicBone_Ver02), "LateUpdate")]
             private static bool DynamicBonesBeforeLateUpdate(ref MonoBehaviour __instance) {
                 bool result;
                 // If we don't optimise, then always run the Update scripts
                 if (!Performancer.OptimiseGuideObjectLate.Value || !Performancer.OptimiseDynamicBones.Value) {
                     result = true;
-                // If some value has changed, start running
+                    // If some value has changed, start running
                 } else if (
                     (__instance is DynamicBone db && (
+                        (db.m_Particles.Count > 0 && dicDynBoneVals[__instance]["tfPos"] != db.m_Particles[Mathf.Max(db.m_Particles.Count - 2, 0)].m_Transform.position) ||
                         dicDynBoneVals[__instance]["force"] != db.m_Force ||
                         dicDynBoneVals[__instance]["gravity"] != db.m_Gravity
                     )) ||
+                    (__instance is DynamicBone_Ver01 db1 && (
+                        (db1.m_Particles.Count > 0 && dicDynBoneVals[__instance]["tfPos"] != db1.m_Particles[Mathf.Max(db1.m_Particles.Count - 2, 0)].m_Transform.position) ||
+                        dicDynBoneVals[__instance]["force"] != db1.m_Force ||
+                        dicDynBoneVals[__instance]["gravity"] != db1.m_Gravity
+                    )) ||
                     (__instance is DynamicBone_Ver02 db2 && (
+                        (db2.Particles.Count > 0 && dicDynBoneVals[__instance]["tfPos"] != db2.Particles[Mathf.Max(db2.Particles.Count - 2, 0)].Transform.position) ||
                         dicDynBoneVals[__instance]["force"] != db2.Force ||
                         dicDynBoneVals[__instance]["gravity"] != db2.Gravity
                     ))
@@ -157,20 +174,39 @@ namespace Performancer {
                 switch (__instance) {
                     case DynamicBone db:
                         dicDynBoneVals[__instance]["pos"] = db.m_Particles.Count > 0 ? db.m_Particles[db.m_Particles.Count - 1].m_Position : Vector3.zero;
+                        dicDynBoneVals[__instance]["tfPos"] = db.m_Particles.Count > 0 ? db.m_Particles[Mathf.Max(db.m_Particles.Count - 2, 0)].m_Transform.position: Vector3.zero;
+                        dicDynBoneVals[__instance]["force"] = db.m_Force;
+                        dicDynBoneVals[__instance]["gravity"] = db.m_Gravity;
+                        break;
+                    case DynamicBone_Ver01 db:
+                        dicDynBoneVals[__instance]["pos"] = db.m_Particles.Count > 0 ? db.m_Particles[db.m_Particles.Count - 1].m_Position : Vector3.zero;
+                        dicDynBoneVals[__instance]["tfPos"] = db.m_Particles.Count > 0 ? db.m_Particles[Mathf.Max(db.m_Particles.Count - 2, 0)].m_Transform.position : Vector3.zero;
                         dicDynBoneVals[__instance]["force"] = db.m_Force;
                         dicDynBoneVals[__instance]["gravity"] = db.m_Gravity;
                         break;
                     case DynamicBone_Ver02 db:
                         dicDynBoneVals[__instance]["pos"] = db.Particles.Count > 0 ? db.Particles[db.Particles.Count - 1].Position : Vector3.zero;
+                        dicDynBoneVals[__instance]["tfPos"] = db.Particles.Count > 0 ? db.Particles[Mathf.Max(db.Particles.Count - 2, 0)].Transform.position : Vector3.zero;
                         dicDynBoneVals[__instance]["force"] = db.Force;
                         dicDynBoneVals[__instance]["gravity"] = db.Gravity;
                         break;
                 }
 
+                // Have to call these methods when disabling the calculations, otherwise the bones jump back to default positions
                 if (!result) {
                     switch (__instance) {
-                        case DynamicBone db: db.ApplyParticlesToTransforms(); break;
-                        case DynamicBone_Ver02 db: db.ApplyParticlesToTransforms(); break;
+                        case DynamicBone db:
+                            db.InitTransforms();
+                            db.ApplyParticlesToTransforms();
+                            break;
+                        case DynamicBone_Ver01 db:
+                            db.InitTransforms();
+                            db.ApplyParticlesToTransforms();
+                            break;
+                        case DynamicBone_Ver02 db:
+                            db.InitTransforms();
+                            db.ApplyParticlesToTransforms();
+                            break;
                     }
                 }
 
@@ -179,6 +215,7 @@ namespace Performancer {
 
             [HarmonyPostfix]
             [HarmonyPatch(typeof(DynamicBone), "LateUpdate")]
+            [HarmonyPatch(typeof(DynamicBone_Ver01), "LateUpdate")]
             [HarmonyPatch(typeof(DynamicBone_Ver02), "LateUpdate")]
             private static void DynamicBonesAfterUpdate(ref MonoBehaviour __instance) {
                 // If we don't optimise, then skip the postfix
@@ -190,6 +227,9 @@ namespace Performancer {
                 Vector3 pos = Vector3.zero;
                 switch (__instance) {
                     case DynamicBone db:
+                        if (db.m_Particles.Count > 0) pos = db.m_Particles[db.m_Particles.Count - 1].m_Position;
+                        break;
+                    case DynamicBone_Ver01 db:
                         if (db.m_Particles.Count > 0) pos = db.m_Particles[db.m_Particles.Count - 1].m_Position;
                         break;
                     case DynamicBone_Ver02 db:
@@ -207,6 +247,7 @@ namespace Performancer {
 
             [HarmonyPostfix]
             [HarmonyPatch(typeof(DynamicBone), "OnEnable")]
+            [HarmonyPatch(typeof(DynamicBone_Ver01), "OnEnable")]
             [HarmonyPatch(typeof(DynamicBone_Ver02), "OnEnable")]
             private static void DynamicBonesAfterOnEnable(ref MonoBehaviour __instance) {
                 // If we don't optimise, then skip the postfix
--- src/Performancer.Core/Performancer.cs
+++ src/Performancer.Core/Performancer.cs
@@ -28,6 +28,9 @@ namespace Performancer {
         internal static bool isLogCoroutine = false;
         internal static int numGuideObjectLateUpdates = 0;
 
+        private static MonoBehaviour selectDynBone = null;
+        private static Dictionary<DynamicBoneCollider, Dictionary<string, object>> dicColliderVals = new Dictionary<DynamicBoneCollider, Dictionary<string, object>>();
+
         private void Awake() {
             Instance = this;
 
@@ -39,6 +42,13 @@ namespace Performancer {
         }
 
         private void Update() {
+            DoLogging();
+            if (OptimiseGuideObjectLate.Value && OptimiseDynamicBones.Value && HookPatch.Hooks.dicDynBoneVals.Count > 0) {
+                CheckDynBoneColliders();
+            }
+        }
+
+        private void DoLogging() {
             if (!isLogCoroutine && DoLogs.Value) {
                 isLogCoroutine = true;
                 StartCoroutine(LogCoroutine());
@@ -48,6 +58,109 @@ namespace Performancer {
                     Log($"GuideObject LateUpdates this second: {numGuideObjectLateUpdates}");
                     numGuideObjectLateUpdates = 0;
                     isLogCoroutine = false;
+                }
+            }
+        }
+
+        private void CheckDynBoneColliders() {
+            // Make sure we have a valid, active bone selected, if one exists in the scene
+            if (selectDynBone == null || selectDynBone.IsDestroyed()) {
+                selectDynBone = FindObjectOfType<DynamicBone_Ver02>();
+                if (selectDynBone == null) {
+                    selectDynBone = FindObjectOfType<DynamicBone>();
+                }
+                if (selectDynBone == null) {
+                    selectDynBone = FindObjectOfType<DynamicBone_Ver01>();
+                }
+                if (selectDynBone == null) {
+                    HookPatch.Hooks.ClearDynBoneDics();
+                }
+            }
+
+            // Check the colliders
+            if (selectDynBone != null) {
+                List<DynamicBoneCollider> colliders = null;
+                switch (selectDynBone) {
+                    case DynamicBone db: colliders = db.m_Colliders; break;
+                    case DynamicBone_Ver01 db: colliders = db.m_Colliders; break;
+                    case DynamicBone_Ver02 db: colliders = db.Colliders; break;
+                }
+                if (colliders != null && colliders.Count > 0) {
+                    foreach (DynamicBoneCollider collider in colliders) {
+                        if (collider.isActiveAndEnabled) {
+                            // Add collider to dict if it doesn't exist
+                            if (!dicColliderVals.ContainsKey(collider)) {
+                                dicColliderVals.Add(collider, new Dictionary<string, object> {
+                                    { "pos", Vector3.zero },
+                                    { "rot", Quaternion.identity },
+                                    { "scale", Vector3.zero },
+                                    { "center", Vector3.zero },
+                                    { "radius", 0f },
+                                    { "height", 0f },
+                                    { "bound", DynamicBoneCollider.Bound.Inside },
+                                    { "direction", DynamicBoneCollider.Direction.X },
+                                });
+                            }
+
+                            // Check if the collider has changed
+                            var vals = dicColliderVals[collider];
+                            if (
+                                (vals["pos"] is Vector3 pos && pos != collider.transform.position) ||
+                                (vals["rot"] is Quaternion rot && rot != collider.transform.rotation) ||
+                                (vals["scale"] is Vector3 scale && scale != collider.transform.lossyScale) ||
+                                (vals["center"] is Vector3 center && center != collider.m_Center) ||
+                                (vals["radius"] is float radius && radius != collider.m_Radius) ||
+                                (vals["height"] is float height && height != collider.m_Height) ||
+                                (vals["bound"] is DynamicBoneCollider.Bound bound && bound != collider.m_Bound) ||
+                                (vals["direction"] is DynamicBoneCollider.Direction direction && direction != collider.m_Direction)
+                            ) {
+                                // Check each bone if they're within relevant distance, and activate them if so
+                                var destroyedBones = new List<MonoBehaviour>();
+                                float collR = Mathf.Max(collider.m_Height, collider.m_Radius) * collider.transform.lossyScale.magnitude;
+                                foreach (var bone in HookPatch.Hooks.dicDynBoneVals.Keys) {
+                                    if (bone.IsDestroyed()) {
+                                        destroyedBones.Add(bone);
+                                    } else {
+                                        if (HookPatch.Hooks.dicDynBonesToUpdate[bone] == 0) {
+                                            float boneR = 0f;
+                                            Vector3 bonePos = Vector3.zero;
+                                            switch (bone) {
+                                                case DynamicBone db:
+                                                    boneR = 2 * db.m_Radius;
+                                                    if (db.m_Particles.Count > 0) bonePos = db.m_Particles[Mathf.Max(db.m_Particles.Count - 2, 0)].m_Transform.position;
+                                                    break;
+                                                case DynamicBone_Ver01 db:
+                                                    boneR = 2 * db.m_Radius;
+                                                    if (db.m_Particles.Count > 0) bonePos = db.m_Particles[Mathf.Max(db.m_Particles.Count - 2, 0)].m_Transform.position;
+                                                    break;
+                                                case DynamicBone_Ver02 db:
+                                                    if (db.Particles.Count > 0) boneR = 2 * db.Particles[db.Particles.Count - 1].Radius;
+                                                    if (db.Particles.Count > 0) bonePos = db.Particles[Mathf.Max(db.Particles.Count - 2, 0)].Transform.position;
+                                                    break;
+                                            }
+                                            if ((collider.transform.position + collider.m_Center - bonePos).sqrMagnitude <= (collR + boneR)*(collR + boneR)) {
+                                                HookPatch.Hooks.dicDynBonesToUpdate[bone] = HookPatch.Hooks.frameAllowance;
+                                            }
+                                        }
+                                    }
+                                }
+                                foreach (var bone in destroyedBones) {
+                                    HookPatch.Hooks.dicDynBoneVals.Remove(bone);
+                                    HookPatch.Hooks.dicDynBonesToUpdate.Remove(bone);
+                                }
+                            }
+
+                            // Save current collider values
+                            dicColliderVals[collider]["pos"] = collider.transform.position;
+                            dicColliderVals[collider]["rot"] = collider.transform.rotation;
+                            dicColliderVals[collider]["scale"] = collider.transform.lossyScale;
+                            dicColliderVals[collider]["center"] = collider.m_Center;
+                            dicColliderVals[collider]["radius"] = collider.m_Radius;
+                            dicColliderVals[collider]["height"] = collider.m_Height;
+                            dicColliderVals[collider]["bound"] = collider.m_Bound;
+                            dicColliderVals[collider]["direction"] = collider.m_Direction;
+                        }
+                    }
                 }
             }
         }
