ea5abaf65c158069c210ed19d505097d1e6c9e86
starstormhun <markus.tamas98@gmail.com>
2024-12-29 01:10:01 +0100

    Add A12 compatibility

--- src/AccMover.Core/AccMover.cs
+++ src/AccMover.Core/AccMover.cs
@@ -1,8 +1,9 @@
 using BepInEx;
-using BepInEx.Configuration;
 using ChaCustom;
-using KKAPI.Utilities;
 using UnityEngine;
+using KKAPI.Utilities;
+using BepInEx.Configuration;
+using System.Collections.Generic;
 
 [assembly: System.Reflection.AssemblyFileVersion(AccMover.AccMover.Version)]
 
@@ -23,32 +24,62 @@ namespace AccMover {
         public static AccMover Instance { get; private set; }
 
         public static ConfigEntry<bool> IsDebug { get; private set; }
+        public static ConfigEntry<bool> IsTest { get; private set; }
+
+        internal static CvsAccessoryChange _cvsAccessoryChange;
+
+        internal static HashSet<int> selected = new HashSet<int>();
 
         private void Awake() {
             Instance = this;
 
             IsDebug = Config.Bind("General", "Debug", false, new ConfigDescription("Log debug messages", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
+            IsTest = Config.Bind("General", "Test", false, new ConfigDescription("This shouldn't be here, please report", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
+
+            KKAPI.Maker.MakerAPI.MakerFinishedLoading += (x, y) => {
+                _cvsAccessoryChange = GameObject.Find("04_AccessoryTop").GetComponentInChildren<CvsAccessoryChange>(true);
+            };
 
             HookPatch.Init();
 
             if (IsDebug.Value) Log("Awoken!");
         }
 
+        // TODO modify for loops with custom slot selection
         private static void DoTransfer() {
             var transfer = GameObject.Find("CustomScene/CustomRoot/FrontUIGroup/CustomUIGroup/CvsMenuTree/04_AccessoryTop/tglChange/ChangeTop").GetComponent<CvsAccessoryChange>();
+            HookPatch.Conditionals.savedMoveParentage.Clear();
+            // Disable heavy functions on acc copying
             HookPatch.Hooks.disableTransferFuncs = true;
-            for (int i = 0; i < 3; i++) {
-                transfer.selSrc = i;
-                transfer.selDst = i + 4;
+            // Setup dictionary of slot movement
+            // TODO
+            var dicMovement = new Dictionary<int, int>();
+            for (int i = 0; i <= 3; i++) {
+                dicMovement[i] = i + 5;
+            }
+            // Prepare data for handling after accessory copy / movement
+            foreach (int i in dicMovement.Keys) {
+                if (HookPatch.Conditionals.A12) HookPatch.Conditionals.HandleA12Before(i, dicMovement, IsTest.Value);
+            }
+            // Move accessories
+            foreach (var kvp in dicMovement) {
+                transfer.selSrc = kvp.Key;
+                transfer.selDst = kvp.Value;
                 transfer.CopyAcs();
             }
+            // Reenable heavy functions
             HookPatch.Hooks.disableTransferFuncs = false;
+            // Perform copying post-work
             transfer.chaCtrl.AssignCoordinate((ChaFileDefine.CoordinateType)transfer.chaCtrl.fileStatus.coordinateType);
             transfer.chaCtrl.Reload(false, true, true, true);
             transfer.CalculateUI();
             transfer.cmpAcsChangeSlot.UpdateSlotNames();
-            for (int i = 0; i < 3; i++) {
-                Singleton<CustomBase>.Instance.SetUpdateCvsAccessory(i + 4, true);
+            // Handle prepared data
+            foreach (int i in dicMovement.Keys) {
+                Singleton<CustomBase>.Instance.SetUpdateCvsAccessory(i + 5, true);
+            }
+            foreach (int i in dicMovement.Keys) {
+                if (HookPatch.Conditionals.A12) HookPatch.Conditionals.HandleA12After(i);
             }
         }
 
--- src/AccMover.Core/HookPatch.cs
+++ src/AccMover.Core/HookPatch.cs
@@ -1,9 +1,12 @@
+using BepInEx;
 using ChaCustom;
 using HarmonyLib;
 using UnityEngine;
+using System.Linq;
+using AAAAAAAAAAAA;
 using UnityEngine.UI;
+using Illusion.Extensions;
 using System.Collections.Generic;
-using BepInEx;
 
 namespace AccMover {
     public static class HookPatch {
@@ -19,6 +22,7 @@ namespace AccMover {
             // Setup Harmony and patch methods
             public static void SetupHooks() {
                 _harmony = Harmony.CreateAndPatchAll(typeof(Hooks), null);
+                
             }
 
             // Disable Harmony patches of this plugin
@@ -51,6 +55,153 @@ namespace AccMover {
             [HarmonyPatch(typeof(CustomBase), "SetUpdateCvsAccessory")]
             private static bool CustomBaseBeforeSetUpdateCvsAccessory() {
                 return !disableTransferFuncs;
+            }
+        }
+
+        public static class Conditionals {
+            public static bool A12 { get; private set; } = false;
+
+            internal static Dictionary<int, List<string>> savedMoveParentage = new Dictionary<int, List<string>>();
+
+            internal static void Setup() {
+                var plugins = AccMover.Instance.GetComponents<BaseUnityPlugin>();
+                foreach (var plugin in plugins) {
+                    switch (plugin.Info.Metadata.GUID) {
+                        case "starstorm.aaaaaaaaaaaa": A12 = true; break;
+                    }
+                }
+            }
+
+            internal static void HandleA12Before(int slot1, Dictionary<int, int> dicMovement, bool moving) {
+                if (!A12) return;
+                if (AccMover.IsDebug.Value) AccMover.Instance.Log($"Preparing data for slot {slot1}...");
+                if (!dicMovement.ContainsKey(slot1)) {
+                    if (AccMover.IsDebug.Value) AccMover.Instance.Log("This slot was not found in the movement dictionary!", 2);
+                    return;
+                }
+                DoHandleA12Before();
+                void DoHandleA12Before() {
+                    // If we're copying / moving an accessory AND also its parent
+                    if (
+                        AAAAAAAAAAAA.AAAAAAAAAAAA.TryGetMakerAccBone(slot1, out var currentBone) &&
+                        AAAAAAAAAAAA.AAAAAAAAAAAA.dicMakerModifiedParents.TryGetValue(AAAAAAAAAAAA.AAAAAAAAAAAA.coordinateDropdown.value, out var dicCoord)
+                    ) {
+                        if (dicCoord.TryGetValue(slot1, out var originalHash) && AAAAAAAAAAAA.AAAAAAAAAAAA.dicMakerHashBones.TryGetValue(originalHash, out Bone originalParent)) {
+                            // Check for a moving parent
+                            int idxParent = -1;
+                            Bone boneParent = null;
+                            foreach (int i in dicMovement.Keys) {
+                                if (AAAAAAAAAAAA.AAAAAAAAAAAA.TryGetMakerAccBone(i, out var bone)) {
+                                    if (dicMovement.ContainsKey(i)) {
+                                        bool isChild = false;
+                                        Bone current = originalParent;
+                                        while (current != null && !current.bone.name.StartsWith("ca_slot")) {
+                                            if (current.parent == bone) {
+                                                isChild = true;
+                                                break;
+                                            }
+                                            current = current.parent;
+                                        }
+                                        if (isChild) {
+                                            if (AccMover.IsDebug.Value) AccMover.Instance.Log("Found parent!");
+                                            boneParent = bone;
+                                            idxParent = i;
+                                        }
+                                    }
+                                }
+                            }
+
+                            // If the parent is moving, then save parent structure
+                            if (dicMovement.ContainsKey(idxParent) && boneParent != null) {
+                                var savedParents = new List<string>();
+                                Transform current = originalParent.bone;
+                                while (!current.name.StartsWith("ca_slot")) {
+                                    savedParents.Add(current.name);
+                                    current = current.parent;
+                                }
+                                // Add as last element the accessory to be parented, and the accessory to be parented to
+                                savedParents.Add($"{dicMovement[slot1]},{dicMovement[idxParent]}");
+                                savedMoveParentage.Add(slot1, savedParents);
+                                if (AccMover.IsDebug.Value) AccMover.Instance.Log($"Added new data: {string.Join("/", savedParents.ToArray())}");
+                            }
+                        }
+
+                        // In case we're moving instead of copying, check for stationary, non-overwritten children
+                        if (moving) {
+                            var stationaryChildren = new Dictionary<Bone, int>();
+                            for (int i = 0; i < AAAAAAAAAAAA.AAAAAAAAAAAA.customAcsChangeSlot.cvsAccessory.Length; i++) {
+                                if (
+                                    dicCoord.ContainsKey(i) && AAAAAAAAAAAA.AAAAAAAAAAAA.TryGetMakerAccBone(i, out var bone) &&
+                                    !dicMovement.ContainsKey(i) && !dicMovement.Values.Contains(i)
+                                ) {
+                                    bool isChild = bone.parent == currentBone;
+                                    Bone current = bone.parent;
+                                    while (current != null && !current.bone.name.StartsWith("ca_slot")) {
+                                        if (current.parent == currentBone) {
+                                            isChild = true;
+                                            break;
+                                        }
+                                        current = current.parent;
+                                    }
+                                    if (isChild) {
+                                        if (AccMover.IsDebug.Value) AccMover.Instance.Log($"Found child accessory in slot#{i}");
+                                        stationaryChildren.Add(bone, i);
+                                    }
+                                }
+                            }
+
+                            // Save parent structure for all stationary, non-overwritten children
+                            foreach (var entry in stationaryChildren) {
+                                var savedParents = new List<string>();
+                                Transform current = entry.Key.bone.parent;
+                                while (!current.name.StartsWith("ca_slot")) {
+                                    savedParents.Add(current.name);
+                                    current = current.parent;
+                                }
+                                // Add as last element the accessory to be parented, and the accessory to be parented to
+                                savedParents.Add($"{entry.Value},{dicMovement[slot1]}");
+                                savedMoveParentage.Add(slot1, savedParents);
+                                if (AccMover.IsDebug.Value) AccMover.Instance.Log($"Added new data: {string.Join("/", savedParents.ToArray())}");
+                            }
+                        }
+                    }
+                }
+            }
+
+            internal static void HandleA12After(int slot1) {
+                if (!A12) return;
+                DoHandleA12After();
+                void DoHandleA12After() {
+                    if (AccMover.IsDebug.Value) AccMover.Instance.Log($"Applying prepared data for slot {slot1}!");
+                    // Apply parenting based on saved data
+                    if (savedMoveParentage.TryGetValue(slot1, out var entry)) {
+                        var entryCopy = new List<string>();
+                        entryCopy.AddRange(entry);
+                        var parentStrings = entryCopy[entryCopy.Count - 1].Split(',');
+                        entryCopy.RemoveAt(entryCopy.Count - 1);
+                        if (parentStrings.Length == 2 && int.TryParse(parentStrings[0], out int idxChild) && int.TryParse(parentStrings[1], out int idxParent)) {
+                            var tfParent = AAAAAAAAAAAA.AAAAAAAAAAAA.chaCtrl.objAccessory?[idxParent]?.transform;
+                            if (tfParent != null) {
+                                string hash;
+                                entryCopy.Reverse();
+                                tfParent = tfParent.Find(string.Join("/", entryCopy.ToArray()));
+                                if (AAAAAAAAAAAA.AAAAAAAAAAAA.dicMakerTfBones.ContainsKey(tfParent)) {
+                                    hash = AAAAAAAAAAAA.AAAAAAAAAAAA.dicMakerTfBones[tfParent].MakeHash(true);
+                                } else {
+                                    var boneParent = new Bone(tfParent);
+                                    hash = boneParent.Hash;
+                                    boneParent.Destroy();
+                                }
+                                int nowCoord = AAAAAAAAAAAA.AAAAAAAAAAAA.coordinateDropdown.value;
+                                if (!AAAAAAAAAAAA.AAAAAAAAAAAA.dicMakerModifiedParents.ContainsKey(nowCoord)) {
+                                    AAAAAAAAAAAA.AAAAAAAAAAAA.dicMakerModifiedParents[nowCoord] = new Dictionary<int, string>();
+                                }
+                                AAAAAAAAAAAA.AAAAAAAAAAAA.dicMakerModifiedParents[nowCoord][idxChild] = hash;
+                                if (AccMover.IsDebug.Value) AccMover.Instance.Log($"Set new parent of {idxChild} to {hash}!");
+                            }
+                        }
+                    }
+                }
             }
         }
     }
