d52a9753bf6d0afe08003d2cdefae5088a0f2087
starstormhun <markus.tamas98@gmail.com>
2024-12-30 17:41:20 +0100

    Increment version to 1.0.0

--- src/AccMover.Core/AccMover.cs
+++ src/AccMover.Core/AccMover.cs
@@ -23,7 +23,7 @@ namespace AccMover {
 	/// </info>
     public class AccMover : BaseUnityPlugin {
         public const string GUID = "starstorm.accmover";
-        public const string Version = "0.1.0." + BuildNumber.Version;
+        public const string Version = "1.0.0." + BuildNumber.Version;
 
         public static AccMover Instance { get; private set; }
 
